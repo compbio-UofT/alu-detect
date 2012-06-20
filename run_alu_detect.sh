e6d94b46a1cbd84be5fd7ff0ca26188d29355f1c
Matei David <matei@cs.toronto.edu>
2012-06-20 14:25:19 -0400

    Removed environment variables from run_alu

--- run_alu_detect.sh
+++ run_alu_detect.sh
@@ -6,5 +6,4 @@ if [[ ! -re "$DIR/settings/settings.hg18" ]]; then
 	exit 1
 fi
 . $DIR/settings/settings.hg18
-#RGID=00 NCPU=24 ORIG_MAPPINGS=/data/matei/experiment/SRX079579-WXS-NA18507/map.sam.gz CONF=a $DIR/bin/alu-detect 2> 00.alu-detect.log
-RGID=00 NCPU=24 ORIG_MAPPINGS=/data/matei/2012.05.28/sim2/mappings/map.sam.gz CONF=a $DIR/bin/alu-detect 2> 00.alu-detect.log
+$DIR/bin/alu-detect
