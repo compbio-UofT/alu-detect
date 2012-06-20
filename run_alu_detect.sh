def865683e235e7f5be1ab27d3a3ec6369841c3a
Matei David <matei@cs.toronto.edu>
2012-06-20 14:17:59 -0400

    Fixed settings directory in run_alu_detect

--- run_alu_detect.sh
+++ run_alu_detect.sh
@@ -5,6 +5,6 @@ if [[ ! -re "$DIR/settings/settings.hg18" ]]; then
 	echo "Please run setup!"
 	exit 1
 fi
-. $DIR/data/settings.hg18
+. $DIR/settings/settings.hg18
 #RGID=00 NCPU=24 ORIG_MAPPINGS=/data/matei/experiment/SRX079579-WXS-NA18507/map.sam.gz CONF=a $DIR/bin/alu-detect 2> 00.alu-detect.log
 RGID=00 NCPU=24 ORIG_MAPPINGS=/data/matei/2012.05.28/sim2/mappings/map.sam.gz CONF=a $DIR/bin/alu-detect 2> 00.alu-detect.log
